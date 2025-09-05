d0a37f96db9f92d785799fb55b2bfc49d5fdcbd3
ebramtawfik <ebramtawfik>
2025-09-04 17:16:54 -0700

    gateways

--- Models/Gateway/VirtualNetworkAzureResource.cs
+++ Models/Gateway/VirtualNetworkAzureResource.cs
@@ -1,24 +1,6 @@
 using System.Text.Json.Serialization;
 
-namespace DataFactory.MCP.Models;
-
-/// <summary>
-/// The public key of the on-premises gateway
-/// </summary>
-public class PublicKey
-{
-    /// <summary>
-    /// The exponent of the public key
-    /// </summary>
-    [JsonPropertyName("exponent")]
-    public string Exponent { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The modulus of the public key
-    /// </summary>
-    [JsonPropertyName("modulus")]
-    public string Modulus { get; set; } = string.Empty;
-}
+namespace DataFactory.MCP.Models.Gateway;
 
 /// <summary>
 /// The properties of a Virtual Network Azure resource
